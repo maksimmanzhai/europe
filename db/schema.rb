fdc745733223f8aa949a5e413594a93bac7792c6
maksimmanzhai <bender23max@gmail.com>
2019-06-11 21:27:23 +0300

    Create table products

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_175124) do
+ActiveRecord::Schema.define(version: 2019_06_11_181849) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
     t.integer "population"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price", precision: 7, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
